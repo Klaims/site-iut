1d268e1677aa1a41d8223eb463f382849877a794
Hugo <blaison.hugo@gmail.com>
2019-05-28 15:22:34 +0200

    Debut header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { AccueilComponent } from './accueil/accueil.component';
+import { ListeComponent } from './liste/liste.component';
+import { RechercheComponent } from './recherche/recherche.component';
+
+const appRoutes : Routes = [
+
+  {path: '', component : AccueilComponent},
+  {path: 'accueil', component: AccueilComponent},
+  {path: 'liste', component: ListeComponent},
+  {path: 'recherche', component: RechercheComponent}
+
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    AccueilComponent,
+    ListeComponent,
+    RechercheComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
